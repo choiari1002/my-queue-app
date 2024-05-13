02bf22e8139f71aa117e89d5a0356faa46b4bdd0
Ari Choi <choiari1002@gmail.com>
2024-05-12 23:31:43 -0700

    add RPC call for restaurant queue count

--- src/app/restaurants/page.tsx
+++ src/app/restaurants/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import { useState, useEffect } from "react";
 import { redirect } from "next/navigation";
@@ -6,17 +5,44 @@ import { Card, MultiSelect, TextInput, Button, Select } from "@mantine/core";
 // import { makeServerClient } from "@/utils/supabaseServerClient.utils";
 import { makeBrowserClient } from "@/utils/supabaseBrowserClient.utils";
 import Link from "next/link";
+import { Restaurant } from "./[restaurantId]/restaurant.component";
 
 const RestaurantsPage = () => {
   const supabase = makeBrowserClient();
+
+  supabase.rpc("restaurant_queue_count").then((response) => {
+    const data = response.data;
+    if (data) {
+      console.log("Returned data:", data);
+    } else {
+      console.log("No data returned.");
+    }
+  });
+
+  // data 결과
+  //   [
+  //     {
+  //         "name": "Bon Ga Korean Restaurant",
+  //         "city": "Surrey",
+  //         "queue_count": 0
+  //     },
+  //     {
+  //         "name": "Starbucks",
+  //         "city": "Surrey",
+  //         "queue_count": 1
+  //     },
+  //     {
+  //         "name": "Anh and Chi",
+  //         "city": "Vancouver",
+  //         "queue_count": 2
+  //     }
+  // ]
+
   // 현재 선택된 지역 상태와 레스토랑 리스트 상태 추가
   const [selectedArea, setSelectedArea] = useState("All");
   const [restaurantList, setRestaurantList] = useState([]);
 
   useEffect(() => {
-    // const {data} = supabase.rpc("restaurant_queue_count")
-    // .then(console.log)
-
     // 선택된 지역에 따라 레스토랑 리스트를 업데이트
     const fetchRestaurants = async () => {
       let query = supabase.from("restaurants").select("*");
