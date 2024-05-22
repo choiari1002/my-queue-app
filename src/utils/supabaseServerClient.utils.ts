70227c97bce2837f06d33a0b1607e3e8786a49d9
Ari Choi <choiari1002@gmail.com>
2024-05-21 22:40:52 -0700

    add middleware - additional updates required

--- src/utils/supabaseServerClient.utils.ts
+++ src/utils/supabaseServerClient.utils.ts
@@ -1,6 +1,8 @@
-// import { Database } from "@/supabase.types";
+import { Database } from "@/supabase.types";
 import { CookieOptions, createServerClient } from "@supabase/ssr";
+import { request } from "http";
 import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
 export const makeServerClient = () => {
   const cookieStore = cookies();
@@ -35,3 +37,60 @@ export const makeServerClient = () => {
     },
   );
 };
+
+export const makeMiddlewareClient = (request: NextRequest) => {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          })
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            }
+          })
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    },
+  );
+
+  return { response, supabase };
+};
+
