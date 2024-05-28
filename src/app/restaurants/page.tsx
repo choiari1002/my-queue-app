68951d840b0bac54c0383e3efe4ef3526dd527d7
Ari Choi <choiari1002@gmail.com>
2024-05-27 23:06:58 -0700

    Revert "add another validation"
    This reverts commit 52dc34230d06af5ef14bb44539538f7d66f075b2.

--- src/app/restaurants/page.tsx
+++ src/app/restaurants/page.tsx
@@ -1,19 +1,17 @@
+"use client"
+
 import { makeServerClient } from "@/utils/supabaseServerClient.utils";
 import { redirect } from "next/navigation";
 import { RestaurantList } from "./restaurantList.component";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation"; // useSearchParams 추가
 
-const RestaurantsPage = async () => {
-  const searchParams = useSearchParams();
-
-  const supabase = makeServerClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user) return redirect("/auth/signin");
-
+const RestaurantsPage = () => {
+  const searchParams = useSearchParams(); // 쿼리 파라미터 가져오기
   const city = searchParams.get("city");
   const regionId = searchParams.get("region");
+
+  console.log(city);
+  console.log(regionId);
 
   return (
     <RestaurantList/>
